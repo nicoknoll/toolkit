b7f342a9f87f9134044a6dc9255abb213a77a337
Nico Knoll <mail@nico.is>
2024-10-27 10:32:52 +0100

    Adjustments

--- js/packages/forms/src/index.ts
+++ js/packages/forms/src/index.ts
@@ -3,3 +3,10 @@ import './index.css';
 export * from './fields';
 export * from './forms';
 export * from './widgets';
+
+export {
+    default as setNativeInputValue,
+    setNativeSelectValue,
+    getNativeSelectValue,
+    setNativeTextareaValue,
+} from './utils/setNativeInputValue';
