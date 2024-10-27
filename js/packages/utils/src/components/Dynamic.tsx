59a4f9dbacd14ac583e30187cb9e0d0cdb3c3436
Nico Knoll <mail@nico.is>
2024-10-27 10:39:03 +0100

    Adjustments

--- js/packages/utils/src/components/Dynamic.tsx
+++ js/packages/utils/src/components/Dynamic.tsx
@@ -4,8 +4,11 @@ type DynamicProps<T extends React.ElementType> = {
     component: T;
 } & React.ComponentPropsWithRef<T>;
 
-const Dynamic = <T extends React.ElementType>({ component: Component, ...props }: DynamicProps<T>) => {
+const Dynamic = <T extends React.ElementType = React.ElementType>({
+    component: Component,
+    ...props
+}: DynamicProps<T>): React.ReactElement => {
     return <Component {...(props as React.ComponentPropsWithRef<T>)} />;
 };
 
-export default Dynamic;
+export default Dynamic as <T extends React.ElementType>(props: DynamicProps<T>) => React.ReactElement;
