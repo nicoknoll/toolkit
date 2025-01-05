d3d257a80bb7088cf512d7dc2a9f07d54c45e5a2
Nico Knoll <mail@nico.is>
2025-01-06 00:55:17 +0100

    Fix calendar dropdown

--- js/packages/forms/src/widgets/TextArea.tsx
+++ js/packages/forms/src/widgets/TextArea.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import Widget, { useWidgetState, WidgetProps } from './Widget.tsx';
 import { classnames } from '@nicoknoll/utils';
 
-export interface TextAreaProps extends React.ComponentPropsWithRef<'textarea'>, WidgetProps {}
+export interface TextAreaProps extends React.ComponentPropsWithRef<'textarea'>, WidgetProps {
+    inputClassName?: string;
+}
 
-const TextArea = ({ className, controls, widgetRef, ref, ...props }: TextAreaProps) => {
+const TextArea = ({ inputClassName, className, controls, widgetRef, ref, ...props }: TextAreaProps) => {
     const [value, onChange] = useWidgetState('', props.value, props.onChange);
 
     return (
@@ -14,7 +16,8 @@ const TextArea = ({ className, controls, widgetRef, ref, ...props }: TextAreaPro
                     ref={ref}
                     className={classnames(
                         'px-2 py-1.5 flex-1 min-w-0 min-h-[4rem] bg-transparent',
-                        props.disabled && 'resize-none'
+                        props.disabled && 'resize-none',
+                        inputClassName
                     )}
                     {...props}
                     value={value}
